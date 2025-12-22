ea4dfdeb10a52f667ec08fe1635427a0b58a0813
lamia <lamia.idir@outlook.fr>
2025-12-22 21:57:32 +0100

    css mobile

--- Frontend/src/Components/Collapse.jsx
+++ Frontend/src/Components/Collapse.jsx
@@ -8,7 +8,7 @@ export default function Collapse({ title, children, content}) {
   return (
     <div className="collapse">
       <button className="collapse__header" onClick={() => setOpen(!open)}>
-        <span>{title}</span>
+        <span className="colapse__title">{title}</span>
         <span className={`collapse__arrow ${open ? "open" : ""}`}><i className="fa-solid fa-chevron-up"></i></span>
       </button>
 
